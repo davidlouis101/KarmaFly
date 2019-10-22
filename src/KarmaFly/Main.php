87eea0f91d78d3beb2c58c7002e41fd5f105b685
davidlouis101 <54008552+davidlouis101@users.noreply.github.com>
2019-10-22 14:12:55 +0200

    Update Main.php

--- src/KarmaFly/Main.php
+++ src/KarmaFly/Main.php
@@ -1,53 +1,35 @@
-?php
+<?php
 
  namespace KarmaFly\Main;
 
  
 
 use pocketmine\event\Listener;
-
 use pocketmine\utils\TextFormat;
-
 use pocketmine\plugin\PluginBase;
-
 use pocketmine\plugin\Plugin;
-
 use pocketmine\command\Command;
-
 use pocketmine\command\CommandSender;
-
 use pocketmine\Player;
-
 use pocketmine\Server;
-
 use pocketmine\utils\Config;
-
  
 
  class Main extends PluginBase implements Listener {
-
    
 
    public $config;
-
    public const PREFIX = "§r[§l§eKarmaFly] §9";
 
    
-
    public function onEnable() {
-
     $this->getServer()->getPluginManager()->registerEvents($this, $this); 
-
     $this->getServer()->getLogger()->warning(TextFormat::BLUE . "Deaktiviert " . TextFormat::GREEN . "Aktiviert.");
-
   }
 
-  
 
   public function onDisable() {
-
-    $this->getServer()->getLogger()->info(TextFormat::BLUE . "Deaktiviert " . TextFormat::RED . "Deaktiviert.");
-
+    $this->getServer()->getLogger()->info(TextFormat::BLUE . "Deaktiviert " . TextFormat::RED . "Deaktiviert.")
   }
 
    
@@ -62,7 +44,7 @@ use pocketmine\utils\Config;
 
     } else {
 
-      $sender->sendMessage(self::PREFIX . "Du kannst nun Fliegen!")
+      $sender->sendMessage(self::PREFIX . "§aDu kannst nun Fliegen!")
 
       $sender->setAllowFlight(true);
 
